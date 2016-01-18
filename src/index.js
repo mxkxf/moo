daeff10922e57936a8a7ae155bfa4347da418ba0
Mike Francis <mikeffrancis@gmail.com>
2016-01-18 09:19:27 +0000

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cowsay = require('cowsay');
-var env = require('./../env');
+var env = require('dotenv');
+
+require('dotenv').load();
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.post('/store', function (req, res) {
+	if (req.body.token !== process.env.SLACK_TOKEN) {
+		return false;
+	}
+
 	return res.send(cowsay.say({ text: req.body.text }));
 });
 
