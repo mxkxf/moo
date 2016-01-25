e9e3aaf02981a5aff0654adc143a2ad0a733bc79
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2016-01-25 07:46:15 +0100

    fix all jscs found problems

--- src/index.js
+++ src/index.js
@@ -8,27 +8,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 3000);
 
-app.post('/', function (req, res) {
-	if (req.body.token !== process.env.SLACK_TOKEN) {
-		return res.status(400)
-			.send({ text: "Slack token is incorrect" });
-	}
+app.post('/', function(req, res) {
+  if (req.body.token !== process.env.SLACK_TOKEN) {
+    return res.status(400)
+    .send({ text: 'Slack token is incorrect' });
+  }
 
-	if (!req.body.text) {
-		return res.status(400)
-			.send({ text: "No text provided" });
-	}
+  if (!req.body.text) {
+    return res.status(400)
+    .send({ text: 'No text provided' });
+  }
 
-	var eyes = req.body.e || req.body.eyes;
-	var tongue = req.body.T || req.body.tongue;
+  var eyes = req.body.e || req.body.eyes;
+  var tongue = req.body.T || req.body.tongue;
 
-	var response = '```' + cowsay.say({ text: req.body.text, e: eyes, T: tongue }) + '```';
-	return res.send(response);
+  var response = '```' + cowsay.say({ text: req.body.text, e: eyes, T: tongue }) + '```';
+  return res.send(response);
 });
 
-app.all('*', function (req, res) {
-	return res.status(400)
-		.send({ text: "Error: Please check your Slash Command's Integration URL" });
+app.all('*', function(req, res) {
+  return res.status(400)
+  .send({ text: "Error: Please check your Slash Command's Integration URL" });
 });
 
 module.exports = app;
